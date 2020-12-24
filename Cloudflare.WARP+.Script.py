c32e31ed017c68fead4f5631b91118d932cc3425
Inspector William Henderson <InspectorWilliamHenderson@gmail.com>
2020-12-24 16:09:23 +0530

    Update Heroku Stack to v20 (#1)

--- Cloudflare.WARP+.Script.py
+++ Cloudflare.WARP+.Script.py
@@ -16,9 +16,21 @@ url = f'https://api.cloudflareclient.com/v0a{digitString(3)}/reg'
 def run():
 	try:
 		install_id = genString(22)
-		body = {"key": "{}=".format(genString(43)),"install_id": install_id,"fcm_token": "{}:APA91b{}".format(install_id, genString(134)),"referrer": referrer,"warp_enabled": False,"tos": datetime.datetime.now().isoformat()[:-3] + "+02:00","type": "Android","locale": "es_ES"}
+		body = {"key": "{}=".format(genString(43)),
+				"install_id": install_id,
+				"fcm_token": "{}:APA91b{}".format(install_id, genString(134)),
+				"referrer": referrer,
+				"warp_enabled": False,
+				"tos": datetime.datetime.now().isoformat()[:-3] + "+02:00",
+				"type": "Android",
+				"locale": "es_ES"}
 		data = json.dumps(body).encode('utf8')
-		headers = {'Content-Type': 'application/json; charset=UTF-8','Host': 'api.cloudflareclient.com','Connection': 'Keep-Alive','Accept-Encoding': 'gzip','User-Agent': 'okhttp/3.12.1'}
+		headers = {'Content-Type': 'application/json; charset=UTF-8',
+					'Host': 'api.cloudflareclient.com',
+					'Connection': 'Keep-Alive',
+					'Accept-Encoding': 'gzip',
+					'User-Agent': 'okhttp/3.12.1'
+					}
 		req = urllib.request.Request(url, data, headers)
 		response = urllib.request.urlopen(req)
 		status_code = response.getcode()
@@ -36,5 +48,5 @@ while True:
 		time.sleep(30)
 	else:
 		b = b + 1
-		print("Error When Connecting to Server.")
 		print(f"[#] Total: {g} Good {b} Bad")
+		time.sleep(30)
